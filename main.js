b556cf6697a9953adc109a76ea1894c7fb4381d7
jjaryupwork <jjaryupwork@gmail.com>
2017-05-30 15:52:24 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var element = document.getElementById('content');
 
 function dropdownStyles(e) {
-  if (e.target.tagName.toUpperCase() !== 'SELECT') {
+  if (e.target.tagName !== 'SELECT') {
     return;
   }
-  e.stopPropagation();
   element.style[e.target.id] = e.target.value;
 }
 document.body.addEventListener('change', dropdownStyles, false);
